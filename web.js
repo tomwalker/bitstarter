2abdd3be975aa3d077bc09a1faa4f99664221efc
Tom Walker <tomwalker0472@gmail.com>
2013-07-05 23:24:52 +0100

    trying to read file contents to web.js

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
+var fs = require('fs');
+
+fs.readFile('/index.html', function(err, data){
+	if (err) throw err;
+	var buff = new Buffer(data);
+});
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
